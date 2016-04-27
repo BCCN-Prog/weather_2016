63a43843d739a9fbd7afa82902b26ae2c6c40e88
Denis Alevi <mail@denisalevi.de>
2016-04-27 17:29:36 +0200

    Update and add try/catch to run_scraping

--- scraping/run_scraping.py
+++ scraping/run_scraping.py
@@ -1,19 +1,30 @@
 """
 Python script to download all html files from all providers.
 Import all providers you want to download from and call their download() function to start the downloads.
+If you want to add a new provider, import it AND add it to provider_list.
 """
 
 import os
 
+import accuweather
 import wetter_com
 import owm
 import wetter_de
+
+provider_list = [wetter_com, 
+                 owm, 
+                 wetter_de,
+                 accuweather]
 
 data_path = './data/'
 
 if not os.path.exists(data_path):
     os.makedirs(data_path)
 
-wetter_com.download(data_path)
-owm.download(data_path)
-wetter_de.download(data_path)
+for provider in provider_list:
+    print('RUNNING', provider)
+    try:
+        provider.download(data_path)
+    except:
+        err = sys.exc_info()[0]
+        print('Excepted error in {}.\n\n{}'.format(provider, err))
