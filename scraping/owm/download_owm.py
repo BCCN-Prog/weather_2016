3da64b208a9869d20cea84faeba95d5bca617fb2
Claus Lang <clauslang@yahoo.de>
2016-05-03 17:26:13 +0200

    changed script so it waits for JS being loaded and then downloads actually useful data. uses selenium

--- scraping/owm/download_owm.py
+++ scraping/owm/download_owm.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
-import urllib.request
-from time import strftime
+#import urllib.request
+import time
+from selenium import webdriver  
+from bs4 import BeautifulSoup
 
 base_url = 'http://openweathermap.org/city/{}'
 base_path = '{}owm_{}_{}.html'
@@ -27,11 +29,29 @@ cities = {'berlin': 2950159,
           'freiburg': 2925177,
           'erfurt': 2929670}
 
+
 def download(data_path):
     for city in cities:
+        print('downloading html for {}'.format(city))
         url = base_url.format(cities[city])
-        path = base_path.format(data_path, strftime('%d_%m_%Y_%H_%M'), city)
-        urllib.request.urlretrieve(url, filename=path)
+        path = base_path.format(data_path, time.strftime('%d_%m_%Y_%H_%M'), city)
+        browser = webdriver.Firefox()  
+        browser.get(url)
+        time.sleep(1)  
+        content = browser.page_source
+        browser.quit()
+        soup = BeautifulSoup(content, 'lxml')
+        print(soup.find(id='date_m'))
+        html = soup.prettify("utf-8")
+        print(path)
+        with open(path, "wb") as file:
+            file.write(html)
+
+#def download_old(data_path):
+ ##   for city in cities:
+   #     url = base_url.format(cities[city])
+    #    path = base_path.format(data_path, time.strftime('%d_%m_%Y_%H_%M'), city)
+     #   urllib.request.urlretrieve(url, filename=path)
     
     
     
