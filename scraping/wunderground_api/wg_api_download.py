da34cd0e804a300cfda47736faa86788f1c285ca
Ines Wichert <ines.m.wichert@gmail.com>
2016-05-17 16:50:39 +0200

    checked that data is found for each location in the list

--- scraping/wunderground_api/wg_api_download.py
+++ scraping/wunderground_api/wg_api_download.py
@@ -5,20 +5,23 @@ import pickle
 
 #f = urllib.request.urlopen('http://api.wunderground.com/api/944b3f3c879d2394/geolookup/forecast10day/q/IA/Cedar_Rapids.json')
 
-loc_list = ['Berlin', 'Munich', 'Hamburg', 'Cologne', 'Frankfurt', 'Stuttgart', 'Bremen', 'Leipzig', 'Hanover', 'Nuremberg', 'Dortmund', 'Dresden', 'Kassel', 'Kiel', 'Bielefeld', 'Saarbrücken', 'Rostock', 'Freiburg', 'Erfurt', 'Magdeburg']
+loc_list = ['Berlin', 'Munich', 'Hamburg', 'Cologne', 'Frankfurt', 'Stuttgart', 'Bremen', 'Leipzig', 'Hannover', 'Nuremberg', 'Dortmund', 'Dresden', 'Kassel', 'Kiel', 'Bielefeld', 'Saarbruecken', 'Rostock', 'Freiburg', 'Erfurt', 'Magdeburg']
 
-
-
-f = urllib.request.urlopen('http://api.wunderground.com/api/944b3f3c879d2394/geolookup/forecast10day/q/Germany/Berlin.json')
-#need to convert byte object to string
-reader = codecs.getreader('utf-8') #how is data encoded? 
-parsed_json = json.load(reader(f)) #
+#download daily 10 day forecast
+for loc in loc_list:
+    f = urllib.request.urlopen('http://api.wunderground.com/api/944b3f3c879d2394/geolookup/forecast10day/q/Germany/'+loc+'.json')
+    #need to convert byte object to string
+    print (type(f))
+    reader = codecs.getreader('utf-8') #how is data encoded? 
+    parsed_json = json.load(reader(f)) #
+    print (parsed_json['location']['city'])
+    
 #
 with open('try_Berlin.pkl', 'wb') as p:
     pickle.dump(parsed_json, p, pickle.HIGHEST_PROTOCOL)
 print ('Done')
 print (parsed_json['location']['city'])
-echo (parsed_json | python -m json.tool())
+#echo (parsed_json | python -m json.tool())
     
 #print (parsed_json)
 #location = parsed_json['location']['city']
