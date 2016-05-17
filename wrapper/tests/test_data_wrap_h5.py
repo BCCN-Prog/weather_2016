806afe24d36a76ce48e861d84ac6055d48a8de7c
Greg Knoll <gregory.knoll@campus.tu-berlin.de>
2016-05-17 18:26:55 +0200

    TST: Checks that databases are the correct size.

--- wrapper/tests/test_data_wrap_h5.py
+++ wrapper/tests/test_data_wrap_h5.py
@@ -1,17 +1,20 @@
-import os
 import unittest
-from wrapper.DataWrapH5py import Daily_DataBase
+from wrapper.DataWrapH5py import Daily_DataBase, Hourly_DataBase
 
 class Test_DataBase(unittest.TestCase):
+    daily_DB = Daily_DataBase('test_daily.hdf5')
+    hourly_DB = Hourly_DataBase('test_hourly.hdf5')
 
     def test_1_daily_create(self):
-        daily_DB = Daily_DataBase()
-        self.assertTrue(os.path.exists(daily_DB.f))
+        self.assertEqual(self.daily_DB.f["weather_data"].shape, (400, 9))
+
+    def test_2_hourly_create(self):
+        self.assertEqual(self.hourly_DB.f["weather_data"].shape, (4000, 10))
 
     # def test_isupper(self):
         # self.assertTrue('FOO'.isupper())
         # self.assertFalse('Foo'.isupper())
-#
+        #
     # def test_split(self):
         # s = 'hello world'
         # self.assertEqual(s.split(), ['hello', 'world'])
