9090334017c7b8b308728c998a86c14c20adedaa
Ines Wichert <ines.m.wichert@gmail.com>
2016-05-17 17:56:52 +0200

    downloads 10 day and hourly forecast for 20 cities

--- scraping/wunderground_api/wg_api_download.py
+++ scraping/wunderground_api/wg_api_download.py
@@ -2,6 +2,7 @@ import urllib.request
 import json
 import codecs
 import pickle
+import time
 
 #f = urllib.request.urlopen('http://api.wunderground.com/api/944b3f3c879d2394/geolookup/forecast10day/q/IA/Cedar_Rapids.json')
 
@@ -9,18 +10,27 @@ loc_list = ['Berlin', 'Munich', 'Hamburg', 'Cologne', 'Frankfurt', 'Stuttgart', 
 
 #download daily 10 day forecast
 for loc in loc_list:
+    #get the json object
     f = urllib.request.urlopen('http://api.wunderground.com/api/944b3f3c879d2394/geolookup/forecast10day/q/Germany/'+loc+'.json')
     #need to convert byte object to string
-    print (type(f))
     reader = codecs.getreader('utf-8') #how is data encoded? 
-    parsed_json = json.load(reader(f)) #
+    parsed_json = json.load(reader(f)) 
+    filename = 'wunderground_10days_{}_{}.pkl'.format(time.strftime('%d_%m_%Y_%H_%M'), loc)
+    with open(filename, 'wb') as p:
+        pickle.dump(parsed_json, p, pickle.HIGHEST_PROTOCOL)
     print (parsed_json['location']['city'])
-    
+    f = urllib.request.urlopen('http://api.wunderground.com/api/944b3f3c879d2394/geolookup/hourly/q/Germany/'+loc+'.json')
+    reader = codecs.getreader('utf-8') #how is data encoded? 
+    parsed_json = json.load(reader(f)) 
+    filename = 'wunderground_hourly_{}_{}.pkl'.format(time.strftime('%d_%m_%Y_%H_%M'), loc)
+    with open(filename, 'wb') as p:
+        pickle.dump(parsed_json, p, pickle.HIGHEST_PROTOCOL)
+
 #
-with open('try_Berlin.pkl', 'wb') as p:
-    pickle.dump(parsed_json, p, pickle.HIGHEST_PROTOCOL)
-print ('Done')
-print (parsed_json['location']['city'])
+#with open('try_Berlin.pkl', 'wb') as p:
+#    pickle.dump(parsed_json, p, pickle.HIGHEST_PROTOCOL)
+#print ('Done')
+#print (parsed_json['location']['city'])
 #echo (parsed_json | python -m json.tool())
     
 #print (parsed_json)
