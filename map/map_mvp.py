f87f673e36d82a49a77ca8cb9c38c3b23155b7a8
MaxKirstein <mkirstein@posteo.de>
2016-05-24 18:06:41 +0200

    mvp version of map

--- map/map_mvp.py
+++ map/map_mvp.py
@@ -3,5 +3,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from map import station_map
+from utils import get_geo_locationas
 
 def map_mvp():
+    
+    coordinates = get_geo_locationas()
+    temperature = 20 * np.random.randn(coordinates.shape[0])
+    station_map(coordinates, temperature)
