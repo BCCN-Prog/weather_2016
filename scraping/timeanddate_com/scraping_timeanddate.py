a8c308ae60337879b753235054525049b4f5a314
Alicja Krzesniak <ala.krzesniak@gmail.com>
2016-05-24 16:08:51 +0200

    Script that returns a dictionary but only hourly or daily (not both in one dict)

--- scraping/timeanddate_com/scraping_timeanddate.py
+++ scraping/timeanddate_com/scraping_timeanddate.py
@@ -2,7 +2,9 @@
 from bs4 import BeautifulSoup
 import re
 
+##  NEED TO PUSH THE NEW VERSION ##
 def get_month(string):
+    """ function extracting month from the string from the table, for date sanity check """
     if 'Apr' in string:
         month = '04'
     elif 'Mai' in string:
@@ -16,52 +18,68 @@ def get_month(string):
     return month
 
 def scrape(file):
+    """scraping data from timeanddate.com/weather, returning one dictionary for both files
+    only for dates in April - July period
+    first index in extended in date = 11.05 if downloaded 10.05
+    first index in hourly when downloading at 17:24 is 18:00
+    checks: if date in filename is date of hourly forecast,
+    if the forecast is for the city that is in the filename
+    if day is between 1-31
+    if rain encoding hasn't change
+    if the first file is hourly"""
+
+    out_dict = {}
     site = 'timeanddate.com/weather'
+    out_dict['site'] = site
     soup = BeautifulSoup(open(file), "lxml")
 
+
     # log the location
-    city = soup.title.string.split(',')[-2][1:]
-    print(city) #city name without space at the beginning
-    dicts = []
+    title = soup.title.string.split(',')[0]
+    city = title.split(' ')[-1]
+    #print(city) #city name without space at the beginning
+    print(city, file)
+    if city.lower() in file:
+        out_dict['city'] = city
+    else:
+        raise Exception('forecast for city different than in the file name')
+    out_dict['daily'] = {}
+    out_dict['hourly'] = {}
+
+    date1 = soup.find_all('tr')[2:3]
+    dates = date1[0].find_all('th')
+    date = '{}-{}-2016'
+    a = dates[0].text[2:]  # gets dates in format i.e. 10. Mai
+    month = get_month(a)
+    b = re.findall(r'\d+', str(dates))
+    if 0 < int(b[-1]) < 32:
+        day = b[-1]
+    else:
+        raise Exception('wrong day')
+    date = date.format(day, month)
+
+    if date in file1:
+        out_dict['date'] = date
+    else:
+        raise Exception('wrong date')
+    #scraping table: soup.find_all('tr')
 
     ## get the daily data
     if 'extended' in soup.title.string:
-        for tr in soup.find_all('tr')[2:-1]:
+        for i, tr in enumerate(soup.find_all('tr')[2:-1]):
             dict ={}
-            dict['site'] = site
-            dict['city'] = city
-
-            #scraping date for the forecast
-            dates = tr.find_all('th')
-            date = '{}-{}-2016'
-            a = dates[0].text[2:] #gets dates in format i.e. 10. Mai
-            month = get_month(a)
-
-            b = re.findall(r'\d+', str(dates))
-            print(b[0])
-            if 0 < int(b[0]) < 32:
-                day = b[0]
-            else:
-                raise Exception('wrong day')
-
-            date = date.format(day,month)
-            dict['date'] = date
-            # scraping the rest of columns in the table
+            # scraping columns in the table
             tds = tr.find_all('td')
             c=list(map(int,re.findall(r'\d+', str(tds)))) #list of all numbers in each row of the table
 
             #interesting values:
-            #c[4] - temp min
-            dict['Low'] = float(c[4])
-            #c[5] - temp max
-            dict['High'] = float(c[5])
-            #c[7] - wind [km/h]
-            wind = round(c[7] * 1000 / 3600., 1)
-            dict['wind_speed'] = float(wind)
-            #c[10] - humidity
-            dict['humidity'] = int(c[10])
-            #c[11] - rain chance
-            dict['rain_chance'] = int(c[11])
+            dict['low'] = float(c[4]) #c[4] - temp min
+            dict['high'] = float(c[5]) #c[5] - temp max
+            wind = round(c[7] * 1000 / 3600., 1) #c[7] - wind [km/h]
+            dict['wind_speed'] = wind
+            dict['humidity'] = float(c[10]) #c[10] - humidity
+            dict['rain_chance'] = float(c[11]) #c[11] - rain chance
+            # rain_amt:
             if len(c) ==19:
                 rain= int(c[-7]) + int(c[-6])/10
             elif len(c) == 17:
@@ -69,39 +87,13 @@ def scrape(file):
             else:
                 raise Exception('scraping failed - change in a site structure')
             dict['rain_amt'] = rain
-            dicts.append(dict)
+            out_dict['daily']["{}".format(str(i+1))] = dict
+
 
     ## get the hourly data
-    else:
-        for tr in soup.find_all('tr')[2:-1]: #for every row in a table
+    elif 'Hourly' in soup.title.string:
+        for j, tr in enumerate(soup.find_all('tr')[2:-1]): #for every row in a table
             dict = {}
-            dict['site'] = site
-            dict['city'] = city
-            dates = tr.find_all('th')
-            a = dates[0].text[8:]
-            #print(a, len(a))
-            if 6 <= len(a) <= 8:
-                date = '{}-{}-2016'
-                month = get_month(a)
-
-                b = re.findall(r'\d+', str(dates)) #finding all numbers
-                #b[0] - hour, b[1] - minutes = 00, b[2] - day
-                if 0 < int(b[2]) < 32:
-                    day = b[2]
-                else:
-                    raise Exception('wrong day')
-
-                hour = dates[0].text[:5]
-            elif len(a)<6:
-                hour = dates[0].text
-            else:
-                raise Exception('wrong date')
-            date = date.format(day, month)
-            dict['date'] = date
-            dict['hour'] = hour
-            #print(dates[0].text[:5])
-            #print(date, hour)
-
             #scraping from columns of the table except the first
             tds = tr.find_all('td')
             c = list(map(int, re.findall(r'[\d]+', str(tds))))  # list of all numbers in each row of the table
@@ -111,22 +103,26 @@ def scrape(file):
                 rain = 0
             dict['rain_amt'] = rain
             # interesting values:
-            # c[4] - temp
-            dict['temp'] = float(c[4])
-            # c[5] - temp feels like
-            # c[6] - wind [km/h]
-            wind = round(c[6] * 1000 / 3600., 1)
+            dict['temp'] = float(c[4]) # c[4] - temp
+            wind = round(c[6] * 1000 / 3600., 1) # c[6] - wind [km/h]
             dict['wind_speed'] = wind
-            # c[9] - humidity
-            dict['humidity'] = int(c[9])
-            # c[10] - rain chance
-            dict['rain_chance'] = int(c[10])
-            dicts.append(dict)
+            dict['humidity'] = float(c[9])# c[9] - humidity
+            dict['rain_chance'] = float(c[10]) # c[10] - rain chance
+            if j<10:
+                j= str(j).zfill(2)
+            else:
+                j = str(j)
+            out_dict['hourly']["{}".format(j)] = dict #index: 00, 01, ..., 23
+    else:
+        raise Exception('wrong input file')
 
-    return dicts
+    #print(len(out_dict['hourly'])) #24
+    return out_dict
 
 if __name__ == '__main__':
-    file1 = "timeanddate_com_10-05-2016_berlin_daily.html"
-    file2 = "timeanddate_com_10-05-2016_bremen_hourly.html"
+    file1 = "timeanddate_com_10-05-2016_kiel_daily.html"
+    file2 = "timeanddate_com_10-05-2016_erfurt_hourly.html"
     d = scrape(file2)
-    print(d)
+    print(d['hourly'])
+
+
