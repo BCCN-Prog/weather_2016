a50f817eb79c31a09c0f75bc1be50d0de2a8a86b
Denis Alevi <mail@denisalevi.de>
2016-05-31 14:48:19 +0200

    Merge map_mvp.py into map.py

--- map/map.py
+++ map/map.py
@@ -1,17 +1,11 @@
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
-#from matplotlib import cm as CM
-#from matplotlib import mlab as ml
 import numpy as np
+import pandas as pd
+from map import station_map
+from utils import get_geo_locations
 
-#lats = np.array([47.813, 50.7827])
-#lons = np.array([8.8493, 6.0941])
-temperature = np.array([20.0, -6.0])
-coordinates = np.array([ [47.813, 8.8493], [50.7827, 6.09419] ])
-
-#def station_map(lats, lons, temperature):
 def station_map(coordinates, temperature, hex_grid_size=(50,50)):
-#    map = Basemap(projection='lcc', lat_0 = 51, lon_0 = 10, resolution = 'i', width = 850000, height = 1000000)
     map = Basemap(projection='tmerc', lat_0=51, lon_0=10, llcrnrlat=47, llcrnrlon=5, urcrnrlat=55, urcrnrlon=16, resolution='i')
     map.drawcoastlines()
     map.drawcountries()
@@ -19,7 +13,16 @@ def station_map(coordinates, temperature, hex_grid_size=(50,50)):
     lats = coordinates[:,0]
     lons = coordinates[:,1]
     x, y  = map(lons, lats)
-#    map.hexbin(x,y)
     map.hexbin(x, y, C = temperature, gridsize=hex_grid_size, linewidth=0.5, edgecolor='k')
     map.colorbar(location='bottom')
     plt.show()
+
+
+def map_mvp():
+    
+    coordinates = get_geo_locations()
+    temperature = 20 * np.random.randn(coordinates.shape[0])
+    station_map(coordinates, temperature, hex_grid_size=(20,20))
+
+if __name__ == '__main__':
+    map_mvp()
