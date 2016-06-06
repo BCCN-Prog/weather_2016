6bdc943f224ee5832f72a1543925746fb64d3f7b
Georg Chechelnizki <georg.chechelnizki@rub.de>
2016-06-06 16:48:44 +0200

    slice function works now, though still no nan handling. It does not return the slice of the matrix by value but rather just the indices fulfilling the criterion, allowing to later just intersect the indices of all slicings and load just one small matrix into memory.

--- query_engine_v2/QueryEngine.py
+++ query_engine_v2/QueryEngine.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append('../wrapper/')
 import DataWrapH5py
+import numpy as np
 
 class QueryEngine:
     daily_params = ['date', 'high'] #only for example
@@ -18,3 +19,34 @@ class QueryEngine:
             #self.hourly.create_presorted(self.hourly_params)
             #above line does not work yet because hourly database does not
             #have categories_dict yet
+    
+        self.dset_dict = {"daily":self.daily, "hourly":self.hourly}
+
+    def slice(self, dset, param, lower, upper):
+        '''
+        Takes dset: "daily" or "hourly", indicates dataset, param: string indicating the 
+        category ("date", "high", "low"...), and an upper and lower bound for the values.
+        Returns not a true slice (so save resources) but rather just the indices of the 
+        original matrix fulfilling these criteria. If we go
+        queryObject.daily[slice("daily", "high", 10, 15)], we get the rows of the original
+        matrix where the temperature lies between 10 and 15 degrees.
+        '''
+        dset = self.dset_dict[dset]
+        param_int = dset.categories_dict[param]
+        
+        ind = dset.get_sort_indices(param_int)
+        
+        lo_ind = np.argmax(dset.f["weather_data"][:,param_int][ind] >= lower)
+        hi_ind = np.argmin(dset.f["weather_data"][:,param_int][ind] <= upper)
+        
+        #nan handling!
+
+        #return dset.f["weather_data"][:][ind][lo_ind:hi_ind]
+
+        #the above line shlould not be used because the returned matrix could
+        #still be very large. Instead we are better of gathering all the 
+        #lower/upper indices first, computing their intersection and only then take a slice
+        
+        
+
+        return ind[lo_ind: hi_ind]
