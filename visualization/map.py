54fa5f3d7c6075d4b46abf963906f4bb5856651f
Denis Alevi <mail@denisalevi.de>
2016-06-07 18:29:45 +0200

    Rename station_map to hexagon_map and start interpolation_map

--- visualization/map.py
+++ visualization/map.py
@@ -1,4 +1,5 @@
-from mpl_toolkits.basemap import Basemap
+from mpl_toolkits.basemap import Basemap, cm
+from matplotlib.mlab import griddata
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -28,7 +29,7 @@ def get_geo_locations():
     return coordinates
 
 
-def station_map(coordinates, temperature, hex_grid_size=(50,50)):
+def hexagon_map(coordinates, temperature, hex_grid_size=(50,50)):
     m = Basemap(projection='tmerc', lat_0=51, lon_0=10, llcrnrlat=47, llcrnrlon=5, urcrnrlat=55, urcrnrlon=16, resolution='i')
     m.drawcoastlines()
     m.drawcountries()
@@ -41,11 +42,53 @@ def station_map(coordinates, temperature, hex_grid_size=(50,50)):
     plt.show()
 
 
+def interpolated_color_map(lats, lons, values, spatial_resolution=0.1, interp='nn', cmap=None):#cm.s3pcpn):
+
+    lat_0 = 51
+    lat_min = 47
+    lat_max = 55
+
+    lon_0 = 10
+    lon_min =  5
+    lon_max = 16
+
+    m = Basemap(projection='tmerc', lat_0=lat_0, lon_0=lon_0, 
+                llcrnrlat=lat_min, llcrnrlon=lon_min, urcrnrlat=lat_max, 
+                urcrnrlon=lon_max, resolution='i')
+
+    m.drawcoastlines()
+    m.drawcountries()
+    m.drawmapboundary()
+
+    lats = np.array(lats)
+    lons = np.array(lons)
+    values = np.array(values)
+   
+    lat_inum = (lat_max - lat_min) / spatial_resolution
+    lon_inum = (lon_max - lon_min) / spatial_resolution
+#    xi = np.linspace(lat_min, lat_max + spatial_resolution, xinum)
+#    yi = np.linspace(lon_min, lon_max + spatial_resolution, yinum)
+    lat_i = np.linspace(lat_min, lat_max, lat_inum)
+    lon_i = np.linspace(lon_min, lon_max, lon_inum)
+    lat_i, lon_i = np.meshgrid(lat_i, lon_i)
+   
+    value_i = griddata(lats, lons, values, lat_i, lon_i, interp=interp)
+
+    lat_grid, lon_grid = m.makegrid(value_i.shape[1], value_i.shape[0])
+    x_grid, y_grid = m(lat_grid, lon_grid)
+    m.contourf(x_grid, y_grid, value_i, cmap=cmap)
+    m.scatter(lats, lons, color='k', s=50, latlon=True)
+   
+    plt.show()
+
+
 def map_mvp():
     
     coordinates = get_geo_locations()
     temperature = 20 * np.random.randn(coordinates.shape[0])
     station_map(coordinates, temperature, hex_grid_size=(20,20))
+    #interpolated_color_map(coordinates[:,0], coordinates[:,1], temperature)
 
 if __name__ == '__main__':
     map_mvp()
+
