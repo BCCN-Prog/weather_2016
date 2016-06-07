091602872849edbf54df1ac0a8e922cf9443c7cc
Georg Chechelnizki <georg.chechelnizki@rub.de>
2016-06-07 13:30:38 +0200

    smart_slice now working for all categories. This means that we now presort wrt to all categories and get the slice based on the intersections of index lists. Nan handling seems fine, as the nans are pushed to the end of the lists and any statement of the form np.nan >= x returns false. H5py behaves differently then numpy when indexing with an empty list, hence it is necessary to include an in clause for the case if the intersection of indices is empty.

--- query_engine_v2/QueryEngine.py
+++ query_engine_v2/QueryEngine.py
@@ -4,7 +4,8 @@ import DataWrapH5py
 import numpy as np
 
 class QueryEngine:
-    daily_params = ['date', 'high'] #only for example
+    daily_params = ['date', 'site', 'station_id', 'high', 'low', 'midday', 'rain_chance', 'rain_amt',
+    'cloud_cover', 'city_ID'] #only for example
     hourly_params = ['date']
 
     def __init__(self, make_new=False):
@@ -29,7 +30,7 @@ class QueryEngine:
         Returns not a true slice (so save resources) but rather just the indices of the 
         original matrix fulfilling these criteria. If we go
         queryObject.daily[slice("daily", "high", 10, 15)], we get the rows of the original
-        matrix where the temperature lies between 10 and 15 degrees.
+        matrix where the (high) temperature lies between 10 and 15 degrees.
         '''
         dset = self.dset_dict[dset]
         param_int = dset.categories_dict[param]
@@ -50,3 +51,63 @@ class QueryEngine:
         
 
         return ind[lo_ind: hi_ind]
+
+    def smart_slice(self, dset, params, lower, upper, return_matrix=True):
+        '''
+        Slices utilizing the presorted indices. After solving the nan issue, all categories will be
+        presorted, such that this function takes care of everything.
+        dset string "hourly" or "daily" specifies the dataset, params is the list of categories involved
+        in the slicing, lower and upper the lists of lower and upper limits corresponding to params.
+        By default returns a matrix sliced according to the above criteria. If return_matrix==False,
+        returns just the indices to be sliced by.
+        '''
+        assert(len(params) == len(lower) and len(lower) == len(upper))
+
+        if dset == "daily":
+            sorted_params = self.daily_params
+        else:
+            sorted_params = self.hourly_params
+
+        dset = self.dset_dict[dset]
+        
+        param_order = list(np.sort([dset.categories_dict[params[i]] for i in range(len(params))]))
+        params = [dset.params_dict[param_order[i]] for i in range(len(param_order))]
+        #here we order the param list according to the dset.params_dict in order to make things easier down the path
+
+        params_intersect = [p for p in params if p in sorted_params]
+        params_intersect_int = [dset.categories_dict[params_intersect[i]] for i in range(len(params_intersect))]
+        hi_lo_indices = [params.index(params_intersect[i]) for i in range(len(params_intersect))]
+        hi_intersect = list(np.array(upper)[hi_lo_indices])
+        lo_intersect = list(np.array(lower)[hi_lo_indices])
+        #modify this to support aliases of params by having dictionary of string to strings
+        
+        dset_names = ["{}_indices".format(params_intersect[i]) for i in range(len(params_intersect))]
+        
+        lo_ind = []
+        hi_ind = []
+        for i in range(len(params_intersect)):
+            lo_ind.append(np.argmax(dset.f["weather_data"][:,params_intersect_int[i]][dset.f[dset_names[i]]]>=lo_intersect[i]))
+            hi_ind.append(np.argmin(dset.f["weather_data"][:,params_intersect_int[i]][dset.f[dset_names[i]]]<=hi_intersect[i]))
+        #getting the slicing indices wrt to all parameters in params_intersect
+
+        set_list = [set(dset.f[dset_names[i]][lo_ind[i]:hi_ind[i]]) for i in range(len(params_intersect))]
+        #make a list of sets of indices. I hope this step is not too computation intensive, I do not know how
+        #to do it differently
+
+        ind = list(set.intersection(*set_list))
+        ind = list(np.sort(ind))
+        #sorting in oder to better comply with h5py.
+
+        if not ind:
+            return "No matching enrties."
+
+        if return_matrix == True:
+            return dset.f["weather_data"][ind,:]
+        else:
+            return ind
+        #1: check which params match daily/hourly_params
+        #2: slice these ways first (manually, without argsort)
+        #3: then slice along the other ways, smartly
+        #meaning: find out the smallest range of values, slice that way, and then
+        #in rising order
+
