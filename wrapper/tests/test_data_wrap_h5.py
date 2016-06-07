8561d641557da7dd9c7ccaee235cefc5f0dbf87c
Greg Knoll <gregory.knoll@campus.tu-berlin.de>
2016-06-07 14:48:49 +0200

    TST: geoloc->station_id and add day to daily_dict

--- wrapper/tests/test_data_wrap_h5.py
+++ wrapper/tests/test_data_wrap_h5.py
@@ -19,15 +19,15 @@ class Test_DataBase(unittest.TestCase):
 
     test_num = 666
     hourly_test_dict = {'date': test_num, 'hour': test_num, 'site': test_num,
-                        'geolocation': test_num, 'temperature': test_num,
+                        'station_id': test_num, 'temperature': test_num,
                         'humidity': test_num, 'wind_speed': test_num,
                         'rain_chance': test_num, 'rain_amt': test_num,
                         'cloud_cover': test_num, 'city_ID': test_num}
 
-    daily_test_dict = {'date': test_num, 'site': test_num, 'geolocation': test_num,
+    daily_test_dict = {'date': test_num, 'site': test_num, 'station_id': test_num,
                        'high': test_num, 'low': test_num, 'midday': test_num,
                        'rain_chance': test_num, 'rain_amt': test_num,
-                       'cloud_cover': test_num, 'city_ID': test_num}
+                       'cloud_cover': test_num, 'city_ID': test_num, 'day': test_num}
 
     full_hourly_dict = {'site': test_num, 'date': test_num, 'city': 'berlin',
                         'hourly': {'00': hourly_test_dict, '01': hourly_test_dict,
