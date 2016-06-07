af492fd577a3e690963f6fe337a1f767b08355c7
Wolfram Höps <wolfram.hoeps@yahoo.de>
2016-06-07 18:31:26 +0200

    Changed GUI, added some events.

--- query_engine/GUI/GUILogic.py
+++ query_engine/GUI/GUILogic.py
@@ -1,26 +1,38 @@
 from PyQt4 import QtCore, QtGui
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
-
 import sys
-
 import gui
+
 
 class MainDialog(QDialog, gui.Ui_MainWindow):
 	
-	def __init__(self, parent = None):
-		super(MainDialog, self).__init__ (parent)
-		self.setupUi(self)
+    def __init__(self, parent = None):
+        super(MainDialog, self).__init__ (parent)
+#        self.ui = self.Ui_MainWindow(self)
 
-app = QApplication(sys.argv)
+    def RecentSelected(self):
+        ListRecent = [self.tr('All'), self.tr('Rain Chance')]
+        ui.ParametersList.clear()
+        ui.ParametersList.addItems(ListRecent)
+        
+    def HistoricalSelected(self):
+        ListHistorical = [self.tr('All'), self.tr('Temperature'), self.tr('Rainfall')]
+        ui.ParametersList.clear()
+        ui.ParametersList.addItems(ListHistorical)
+        
 
-#form = MainDialog()
-#form.show()
-#app.exec_() 
 
 app = QtGui.QApplication(sys.argv)
-form = QtGui.QMainWindow()
-ui = Ui_MainWindow()
+MainWindow = QtGui.QMainWindow()
+ui = MainDialog()
 ui.setupUi(MainWindow)
 MainWindow.show()
+
+
+QtCore.QObject.connect(ui.RecentFlag, QtCore.SIGNAL("clicked()"), ui.RecentSelected)
+QtCore.QObject.connect(ui.HistoricalFlag, QtCore.SIGNAL("clicked()"), ui.HistoricalSelected)
+
+
+
 sys.exit(app.exec_())
