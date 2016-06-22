0a1882784b1576ef1dbf26be98f7294ace13988f
janfb <j.f.boelts@gmail.com>
2016-06-22 23:07:25 +0200

    Fixes #59 by allowing up to 100 days of forecast.
    This is just an greedy fix. We have to think about a convention of how many days to forecast in general. I think our first idea was to collect as many data as possible for every provider.

--- scraping/test_scraper_output.py
+++ scraping/test_scraper_output.py
@@ -50,9 +50,9 @@ def test_daily(data_dic):
 
     # test keys format
     for key in daily_dic.keys():
-        assert(isinstance(key, str))
-        assert(len(key) <= 2)
-        assert(0 <= int(key) <= 20), 'key is'.format(key)
+        assert(isinstance(key, str)), 'days should coded as strings'
+        assert(len(key) <= 2), 'days string must not be longer than 2'
+        assert(0 <= int(key) <= 100), 'key is {}, too a forecast'.format(key)
 
     # for every day
     for day_dic in days_dics:
