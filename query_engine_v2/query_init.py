777db05dd6dc3c2df3dbeae33e87eea098b728b6
Georg Chechelnizki <georg.chechelnizki@rub.de>
2016-07-11 17:48:44 +0200

    typo again

--- query_engine_v2/query_init.py
+++ query_engine_v2/query_init.py
@@ -5,4 +5,4 @@ import os
 os.system('rm cached_data.h5py')
 os.system('rm cachedict.p')
 
-q = QueryEngine.QueryEngine("daily_database.hdf5", "houry_database.hdf5", make_new=True)
+q = QueryEngine.QueryEngine("daily_database.hdf5", "hourly_database.hdf5", make_new=True)
