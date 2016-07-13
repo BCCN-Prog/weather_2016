326e269f5193165916c15707274638095161365f
Wolfram Höps <wolfram.hoeps@yahoo.de>
2016-07-13 23:47:09 +0200

    Commented code, added a readme in query_engine_v2, regarding the GUI.

--- query_engine_v2/GUILogic.py
+++ query_engine_v2/GUILogic.py
@@ -7,6 +7,8 @@ import pandas as pd
 sys.path.append('../query_engine_v2')
 import executor as ex
 
+
+#Some Lists of stuff that the GUI should display
 ListRecentDaily = ['Parameter1','Parameter2']
 ListRecentHourly = ['Parameter1','Parameter2']
 ListHistoricalDaily = ['Maximum Temperature', 'Minimum Temperature', 'Average Temperature', 'Rain Amount', 'Cloud Cover']
@@ -21,12 +23,17 @@ ListStations = StationTable['name'].tolist()
 class MainDialog(QDialog, gui.Ui_MainWindow):
 	
     def __init__(self, parent = None):
+        '''
+        Inherit all the shit from gui.ui, where the GUI architecture lies
+        '''
         super(MainDialog, self).__init__ (parent)
 
         
     def Initialize_Connections(self):
-
-        #ui.StationsList.currentIndexChanged.connect(ui.station_selected)
+        '''
+        Tells the GUI which user actions (e.g. "RecentFlag[...]clicked()") should have which internal effect (e.g. set RecentSelected)
+        '''
+        
         QtCore.QObject.connect(ui.RecentFlag, QtCore.SIGNAL("clicked()"), ui.RecentSelected)
         QtCore.QObject.connect(ui.HistoricalFlag, QtCore.SIGNAL("clicked()"), ui.HistoricalSelected)
 
@@ -34,10 +41,16 @@ class MainDialog(QDialog, gui.Ui_MainWindow):
         QtCore.QObject.connect(ui.DailyFlag, QtCore.SIGNAL("clicked()"), ui.DailySelected)
         ui.SubmitButton.released.connect(ui.Collect_Data)
 
-        #ui.ExitButton.released.connect(QApplication.quit())
-        #QtCore.QObject.connect(ui.ExitButton, SIGNAL(clicked()), SLOT(quit()))
     
     def Collect_Data(self):
+
+        '''
+        Most important part!
+        When the user hits "submit", this function collects all data that were given as input, 
+        initializes the Executor class which knows how to deal with this stuff further, 
+        and calls the get_data function from executor, which will use smart_slice and visualization to create an output. 
+        '''
+        ###Run the other class###
         exec_ = ex.Executor()
 
         #Get Time
@@ -53,14 +66,14 @@ class MainDialog(QDialog, gui.Ui_MainWindow):
             StartingTime = StartingTime[0:2]
             EndingTime = EndingTime[0:2]
         
-        #Get h/d, r/h
+        #Get hourly/daily, recent/hist
         hourly_daily = ('hourly' if ui.HourlyFlag.isChecked() else 'daily')
         recent_hist = ('historical' if ui.HistoricalFlag.isChecked() else 'recent')
-        #get parameter and station (selected in drop-down menu)
+        #Get parameter and station (selected in drop-down menu)
         parameter = ui.ParametersList.currentText()
         station = ui.StationsList.currentText()
-        #pass info!
 
+        ###pass info!###
         exec_.get_data(hourly_daily, recent_hist, parameter, station, StartingDateTime, EndingDateTime, StartingTime, EndingTime)
 
 
@@ -117,11 +130,13 @@ class MainDialog(QDialog, gui.Ui_MainWindow):
                 ui.EndingTime.setEnabled(False)                
 
 
+### All this stuff will call the "MainDialog" class from gui.ui (the stuff that the graphical program created),
+### initializes the gui and displays it. 
+
 app = QtGui.QApplication(sys.argv)
 MainWindow = QtGui.QMainWindow()
 ui = MainDialog()
 ui.setupUi(MainWindow)
 ui.Initialize_Connections()
 MainWindow.show()
-
 sys.exit(app.exec_())
