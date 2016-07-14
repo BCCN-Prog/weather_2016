9164c8cdbb70a01564533401ce62827da532be54
Georg Chechelnizki <georg.chechelnizki@rub.de>
2016-07-14 12:43:44 +0200

    query_init.py now also removes the databases

--- query_engine_v2/query_init.py
+++ query_engine_v2/query_init.py
@@ -2,6 +2,8 @@ import QueryEngine
 import sys
 import os
 
+os.system('rm daily_database.hdf5')
+os.system('rm hourly_database.hdf5')
 os.system('rm cached_data.h5py')
 os.system('rm cachedict.p')
 
